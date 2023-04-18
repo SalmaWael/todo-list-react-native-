8fb42bafad8f7d6fd111173029c8f021ddef749e
SalmaWael <salmawael1212@gmail.com>
2023-04-18 10:19:54 +0200

    first-commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState,useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import registerNNPushToken from 'native-notify';
+
+import Home from './src/screens/Home';
+import ChosenTask from './src/screens/ChosenTask';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  //push notification 
+  registerNNPushToken(7280, 'dp2aHXKCtmbiG17LTRzYpK');
+
+  //global state management
+  const [toDoList,setToDoList] =useState([{id:1,task:'brush your teeth'}]);
+  const [task,setTask] =useState('');
+  const [chosenTask,setChosenTask] =useState('');
+
+  const GlobalState ={
+    toDoList,setToDoList,
+    task,setTask,
+    chosenTask,setChosenTask
+  }
+  //navigation
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name="Home" options={{headerShown:false}}>
+          {props => <Home {...props} GlobalState={GlobalState}/>}
+        </Stack.Screen>
+
+        <Stack.Screen name="ChosenTask" options={{headerShown:false}}>
+          {props => <ChosenTask {...props} GlobalState={GlobalState}/>}
+        </Stack.Screen>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
